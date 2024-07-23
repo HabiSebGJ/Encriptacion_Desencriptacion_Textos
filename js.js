037080978ab78459554a5d9d25cf06178a280a31
haisebGJ <habibsebastiangj@gmail.com>
2024-07-23 13:27:16 -0500

    Encripta

--- js.js
+++ js.js
@@ -1,3 +1,13 @@
 function Encrip(){
-    let text = document.getElementsById('texto').value;
+    let text = document.getElementById('texto').value;
+    let textoED = text
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+    
+    if(text.length != 0){
+        document.getElementById('texto').value = textoED;
+    }
 } 
