f0bb322993c27693f35be9af04e2afe45b0693db
haisebGJ <habibsebastiangj@gmail.com>
2024-07-25 11:17:16 -0500

    Encripta y desencripta

--- js.js
+++ js.js
@@ -8,6 +8,21 @@ function Encrip(){
         .replace(/u/gi, "ufat");
     
     if(text.length != 0){
-        document.getElementById('texto').value = textoED;
+        document.getElementById('texto').value = textoED; 
     }
 } 
+
+function Desencrip(){
+    let text = document.getElementById('texto').value;
+    let textoED = text
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+    
+    if(text.length != 0){
+        document.getElementById('texto').value = textoED; 
+    }
+
+}
