bf2428ae4e624f2dcf3c03cc50003d3a6547044b
haisebGJ <habibsebastiangj@gmail.com>
2024-07-29 17:41:39 -0500

    Encripta y Desencripta

--- js.js
+++ js.js
@@ -11,9 +11,16 @@ function Encrip(){
         .replace(/u/gi, "ufat");
     
     if(text.length != 0){
-        document.getElementById('texto').value = textoED;
+        document.getElementById('texto').value = textoED.toLowerCase();
         mensaje.textContent = "Texto encriptado";
         mensaje1.textContent = " ";
+        muñeco.src="./img/WorkDone.png";
+        
+    }else{
+        muñeco.src = "./img/Muñeco.png";
+        alert('No hay texto a encriptar');
+        mensaje.textContent = "Ingresa un texto";
+        mensaje2.textContent="Ingresa el texto que desees encriptar, desencriptar o copiar.";    
     }
 } 
 
@@ -30,9 +37,32 @@ function Desencrip(){
         .replace(/ufat/gi, "u");
     
     if(text.length != 0){
-        document.getElementById('texto').value = textoED;
+        document.getElementById('texto').value = textoED.toLowerCase();
         mensaje.textContent = "Texto desencriptado ";
         mensaje1.textContent = " ";
+        muñeco.src="./img/WorkDone.png";
+    }else{
+        alert('No hay texto a desencriptar');
+        mensaje.textContent = "Ingresa un texto";
+        muñeco.src = "./img/Muñeco.png";
+        mensaje2.textContent="Ingresa el texto que desees encriptar, desencriptar o copiar.";
     }
 
+}
+
+function CopiarText(){
+    let mensaje = document.getElementById('mensaje1');
+    let mensaje1 = document.getElementById('mensaje2');
+    let text = document.getElementById('texto').value;
+    if(text.length != 0){
+        navigator.clipboard.writeText(text);
+        mensaje.textContent = "Texto copiado ";
+        mensaje1.textContent = " ";
+        muñeco.src="./img/WorkDone.png";
+    }else{
+        alert('No hay texto a copiar');
+        muñeco.src = "./img/Muñeco.png";
+        mensaje.textContent = "Ingresa un texto";
+        mensaje2.textContent="Ingresa el texto que desees encriptar, desencriptar o copiar.";
+    }
 }
